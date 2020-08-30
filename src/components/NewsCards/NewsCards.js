fa7f5111bc5939411b65a1d81198bbb63d697f97
sahilthakur7 <sahilrd07@gmail.com>
2020-08-30 12:34:40 +0530

    Added home screen cards

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -2,8 +2,9 @@ import React from "react";
 import NewsCard from "../NewsCard/NewsCard";
 
 import useStyles from "./styles";
-
 import { Grid, Grow, Typography } from "@material-ui/core";
+
+import { infoCards } from "../../constants/infoCards";
 
 const NewsCards = ({ articles }) => {
   const classes = useStyles();
@@ -17,6 +18,56 @@ const NewsCards = ({ articles }) => {
     });
     return newsCards;
   };
+
+  const renderHomeCards = () => {
+    const homeCards = infoCards.map((infoCard, index) => {
+      return (
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={4}
+          lg={4}
+          className={classes.infoCard}
+          style={{ display: "flex" }}
+        >
+          <div
+            className={classes.card}
+            style={{ backgroundColor: infoCard.color }}
+          >
+            <Typography variant="h5">{infoCard.title}</Typography>
+            {infoCard && infoCard.info ? (
+              <Typography variant="h6">
+                <strong>{infoCard.title.split(" ")[2]}:</strong>
+                <br />
+                {infoCard.info}
+              </Typography>
+            ) : null}
+            <Typography variant="h6">
+              Try Saying: <br />
+              <i>{infoCard.text}</i>
+            </Typography>
+          </div>
+        </Grid>
+      );
+    });
+    return homeCards;
+  };
+
+  if (!articles.length) {
+    return (
+      <Grow in>
+        <Grid
+          container
+          className={classes.container}
+          alignItems="stretch"
+          spacing="3"
+        >
+          {renderHomeCards()}
+        </Grid>
+      </Grow>
+    );
+  }
 
   return (
     <Grow in>
